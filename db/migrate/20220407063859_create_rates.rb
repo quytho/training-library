0f765bae6b10d6d1c4d03768fe559c9cc3971653
blacktho <vanquytho12345@gmail.com>
2022-04-12 10:43:55 +0700

    view list book

--- db/migrate/20220407063859_create_rates.rb
+++ db/migrate/20220407063859_create_rates.rb
@@ -1,7 +1,7 @@
 class CreateRates < ActiveRecord::Migration[6.0]
   def change
     create_table :rates do |t|
-      t.integer :star
+      t.integer :star, default: 0
       t.references :user, foreign_key: true
       t.references :book, foreign_key: true
       
