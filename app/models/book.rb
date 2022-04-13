231e230aeaa4e9c1c017216b10355f4f493cdf73
blacktho <vanquytho12345@gmail.com>
2022-04-13 17:24:13 +0700

    search

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,12 +7,9 @@ class Book < ActiveRecord::Base
   has_many :likes
   has_many :comments
   has_many :rates
-  scope :search_name, ->(name) { where("name LIKE ?", "%#{name}%") 
-    if name.present? }
-  scope :search_author, ->(author_id) { where(author_id: author_id) 
-    if author_id.present? }
-  scope :search_publisher, ->(publisher_id) { where(publisher_id: publisher_id) 
-    if publisher_id.present? }
+  scope :search_name, ->(name) { where("name LIKE ?", "%#{name}%") if name.present? }
+  scope :search_author, ->(author_id) { where(author_id: author_id) if author_id.present? }
+  scope :search_publisher, ->(publisher_id) { where(publisher_id: publisher_id) if publisher_id.present? }
   scope :search, lambda { |params|
     search_name(params[:name])
     .search_author(params[:author_id])
