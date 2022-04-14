9f9cfd91441deb9667b7f0055b98d4388124e652
blacktho <vanquytho12345@gmail.com>
2022-04-14 10:03:22 +0700

    pagination + modify css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.search(params)
+    @books = Book.search(params).paginate(page: 1, per_page: 11)
   end
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,8 @@
 module BooksHelper
+  def find_author(book)
+    author = Author.find(book.author_id)
+  end
+  def find_rate(book)
+    rate = Rate.find(book.id)
+  end
 end
