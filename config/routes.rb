c6344a1377a7e23f8e5df242b943c3ae2528af66
hoangthai <hoangthai150199@gmail.com>
2022-04-14 13:59:54 +0700

    Update code 03 author

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :books
   get 'signup' => 'users#new'
-  get 'author' => 'authors#index'
   resources :users
   resources :authors
 end
