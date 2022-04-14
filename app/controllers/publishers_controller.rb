b7a18789002d19f36a142896e16ee96638af13e4
blacktho <vanquytho12345@gmail.com>
2022-04-14 22:04:48 +0700

    form add

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -2,4 +2,11 @@ class PublishersController < ApplicationController
   def index
     @publishers = Publisher.paginate(page: params[:page], per_page: 10)
   end
+  def new
+    @publisher = Publisher.new
+  end
+  private
+    def user_params
+      params.require(:publisher).permit(:name)
+    end
 end
