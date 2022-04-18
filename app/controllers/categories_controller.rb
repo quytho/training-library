b612bf4893f0af501cc97c9324f9c916bac1a9b5
minhhtien11 <minhtienpeanut@gmail.com>
2022-04-18 08:44:31 +0700

    search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,11 @@ class CategoriesController < ApplicationController
   before_action :get_category, except: [:index, :new]
   
   def index
+    @category = if params[:name]
+      Category.where('name LIKE ?', "%#{params[:name]}%")
+    else
     @category = Category.paginate(page: params[ :page], per_page: 3 )
+    end
   end
 
   def show
@@ -13,10 +17,9 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(categories_params)
-
+    @category = Category.create(category_params)
     if @category.save
-      redirect_to index_category_path
+      redirect_to categories_path 
     else
       render :new
     end
@@ -26,9 +29,8 @@ class CategoriesController < ApplicationController
   end
 
   def update
-
     if @category.update(category_params)
-      redirect_to @category
+      redirect_to categories_path
     else
       render :edit
     end
@@ -36,7 +38,7 @@ class CategoriesController < ApplicationController
 
   def destroy
     @category.destroy
-    redirect_to index_category_path
+    redirect_to categories_path
   end
 
   private
@@ -45,8 +47,7 @@ class CategoriesController < ApplicationController
     end
 
     def get_category
-      @category = Category.find_by(params[:id])
-    
-      redirect_to root_url unless @category
+      @category = Category.find_by(id: params[:id])
+      # redirect_to root_url unless @category
     end
 end
