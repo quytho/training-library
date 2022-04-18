6bfe2b56e50f91a1a2f9220cf261a440c7f3defc
quytho <59798610+quytho@users.noreply.github.com>
2022-04-18 12:56:58 +0700

    Update publisher.rb

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,7 +1,7 @@
 class Publisher < ActiveRecord::Base
   has_many :books, dependent: :destroy
   scope :order_name, -> { order(name: :ASC)}
-  scope :search_name, ->(name) { where("name LIKE ?", "%#{name}%") if name.present? }
+  scope :search_name, ->(name) { where("LOWER(name) LIKE ?", "%#{name}%") if name.present? }
   scope :search, lambda { |params|
     search_name(params[:name])
   }
